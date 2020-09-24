2701d31040146428ba3a0e8faffcc1ad0d950c01
mir <djalilovmir@gmail.com>
2020-09-24 23:28:36 +0300

    adding material ui to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "@material-ui/core";
+import React, { useState } from "react";
+
+import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState(["hello", "hi", "whats up"]);
+
+  const sendMessage = (event) => {
+    event.preventDefault()
+    setMessages([...messages, input]);
+  };
   return (
     <div className="App">
-     
+      <h1>Hello clever programmer🚀</h1>
+      <form >
+        <input
+          value={input}
+          onChange={(event) => setInput(event.currentTarget.value)}
+        />
+        <Button type="submit" onClick={sendMessage}>Send Message</Button>
+      </form>
+      {messages.map((message) => {
+        return <p>{message}</p>;
+      })}
     </div>
   );
 }
